dd0c6be46b6324595b66a533d0a10a7dd779f69c
Xemil85 <heiskaneneemil@gmail.com>
2023-10-19 15:55:38 +0300

    Deleted two buttons because it's unnessessary

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,19 +9,7 @@ public partial class MainPage : ContentPage
 
         UusiPelaaja.Clicked += UusiPelaaja_Clicked;
         EntinenPelaaja.Clicked += EntinenPelaaja_Clicked;
-        //Ristinolla.Clicked += Ristinolla_Clicked;
-        //Vastustaja.Clicked += Vastustaja_Clicked;
 	}
-
-    //private async void Vastustaja_Clicked(object sender, EventArgs e)
-    //{
-    //    await Navigation.PushAsync(new Vastustaja());
-    //}
-
-    //private async void Ristinolla_Clicked(object sender, EventArgs e)
-    //{
-    //    await Navigation.PushAsync(new Ristinolla());
-    //}
 
     private async void EntinenPelaaja_Clicked(object sender, EventArgs e)
     {
