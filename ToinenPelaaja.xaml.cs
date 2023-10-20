cf81636832cddc46bc0f41e531385aff124cde5a
Xemil85 <heiskaneneemil@gmail.com>
2023-10-20 14:57:03 +0300

    Code for selecting another player for listview component

---
+++ ToinenPelaaja.xaml.cs
@@ -0,0 +1,45 @@
+using System.Collections.ObjectModel;
+using System.Text.Json;
+
+namespace final_work;
+
+public partial class ToinenPelaaja : ContentPage
+{
+    public ObservableCollection<Pelaaja> Pelaajat { get; set; }
+    private Pelaaja pelaaja;
+    public ToinenPelaaja(Pelaaja pelaaja)
+	{
+		InitializeComponent();
+        this.pelaaja = pelaaja;
+        Pelaajat = new ObservableCollection<Pelaaja>();
+        LoadPelaajat();
+        BindingContext = this;
+        ValitseToinenPelaaja.Clicked += ValitseToinenPelaaja_Clicked;
+    }
+
+    private async void ValitseToinenPelaaja_Clicked(object sender, EventArgs e)
+    {
+        var selectedPelaaja = (Pelaaja)PelaajatListView.SelectedItem;
+
+        if (selectedPelaaja != null)
+        {
+            Ristinolla ristinollaPage = new Ristinolla(pelaaja, selectedPelaaja);
+            await Navigation.PushAsync(ristinollaPage);
+        }
+    }
+
+    private void LoadPelaajat()
+    {
+        string projectDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+        string fileName = "Pelaajat.json";
+        string filePath = System.IO.Path.Combine(projectDirectory, fileName);
+
+        if (System.IO.File.Exists(filePath))
+        {
+            string jsonData = System.IO.File.ReadAllText(filePath);
+            ObservableCollection<Pelaaja> pelaajat = JsonSerializer.Deserialize<ObservableCollection<Pelaaja>>(jsonData);
+            pelaajat = new ObservableCollection<Pelaaja>(pelaajat.Where(p => (p.Etunimi != "Tietokone") && (p.Etunimi != pelaaja.Etunimi)));
+            Pelaajat = pelaajat;
+        }
+    }
+}
